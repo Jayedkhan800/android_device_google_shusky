7b76a878a48b0944355d4d3b3d049cc1bb107981
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2024-12-24 10:14:40 -0500

    Update and rename lineage_husky.mk to alpha_husky.mk

--- alpha_husky.mk
+++ alpha_husky.mk
@@ -6,7 +6,7 @@
 
 # Inherit some common stuff
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/alpha/config/common_full_phone.mk)
 
 # Inherit device configuration
 DEVICE_CODENAME := husky
@@ -19,7 +19,7 @@ $(call inherit-product, $(DEVICE_PATH)/$(DEVICE_CODENAME)/device-lineage.mk)
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 8 Pro
-PRODUCT_NAME := lineage_$(DEVICE_CODENAME)
+PRODUCT_NAME := alpha_$(DEVICE_CODENAME)
 
 # UDFPS support 
 TARGET_HAS_UDFPS := true
@@ -86,9 +86,8 @@ BUILD_GOOGLE_CONTACTS := true
 BUILD_GOOGLE_DIALER := true
 BUILD_GOOGLE_MESSAGE := true
 
-# Mist OS Flags
-MIST_BUILD_TYPE := OFFICIAL
-MISTOS_MAINTAINER := GamerBoy1234294
+# AlphaDroid Flags
+ALPHA_MAINTAINER := "Matthew Witherell"
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2992
