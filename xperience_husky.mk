5127ebf74cd0b3d8a7763e2f9a5395677e367334
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2024-12-28 00:56:42 -0500

    Update and rename lineage_husky.mk to xperience_husky.mk

--- xperience_husky.mk
+++ xperience_husky.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(DEVICE_PATH)/$(DEVICE_CODENAME)/device-lineage.mk)
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 8 Pro
-PRODUCT_NAME := lineage_$(DEVICE_CODENAME)
+PRODUCT_NAME := xperience_$(DEVICE_CODENAME)
 
 # UDFPS support 
 TARGET_HAS_UDFPS := true
